32d2175e00f98357c64e97baf7c2b82334c7a1d7
Xiao Peng <xiao.peng1006@gmail.com>
2019-04-20 00:44:19 -0700

    Leetcode 973. K Closest Points to Origin

---
+++ K-Closest-Points-to-Origin/src/Solution.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Solution {
+
+    /**
+     * Leetcode 973. K Closest Points to Origin
+     * @param points
+     * @param K
+     * @return
+     */
+    public int[][] kClosest(int[][] points, int K) {
+
+        int[] distances = new int[points.length];
+        for (int i = 0; i < points.length; i++)
+            distances[i] = distance(points[i]);
+
+        Arrays.sort(distances);
+        int distK = distances[K-1];
+
+        int[][] res = new int[K][2];
+        int count = 0;
+        for (int[] point:points) {
+            if (distance(point) <= distK)
+                res[count++] = point;
+        }
+        return res;
+
+    }
+
+    public int distance(int[] point) {
+        return point[0]*point[0] + point[1]*point[1];
+    }
+}
