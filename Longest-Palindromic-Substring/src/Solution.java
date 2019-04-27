3890e3b6705d4a02d48d39244aa774b31dbece82
Xiao Peng <xiao.peng1006@gmail.com>
2019-04-27 10:43:42 -0700

    Leetcode 5. Longest Palindromic Substring     - Slide window approach     - O(n^2) Time complexity     - O(1) Space complexity

--- Longest-Palindromic-Substring/src/Solution.java
+++ Longest-Palindromic-Substring/src/Solution.java
@@ -9,22 +9,27 @@ class Solution {
         if (s.length() == 0 || s == null)
             return "";
         String longestPalindrome = s.substring(0,1);
+        String res = "";
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                String str = s.substring(i, j+1);
-                if (isPalindrome(str))
-                    longestPalindrome = longestPalindrome.length() > str.length() ? longestPalindrome : str;
-            }
+            res = helper(s, i, i);
+            if (res.length() > longestPalindrome.length())
+                longestPalindrome = res;
         }
+
+        for (int i = 0; i < s.length()-1; i++) {
+            res = helper(s, i, i+1);
+            if (res.length() > longestPalindrome.length())
+                longestPalindrome = res;
+        }
+
         return longestPalindrome;
     }
 
-    public boolean isPalindrome(String str) {
-        for (int i = 0; i < str.length()-1; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - i - 1))
-                return false;
+    public String helper(String s, int start, int end) {
+        if (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            return helper(s, start-1, end+1);
         }
-        return true;
+        return s.substring(start+1, end);
     }
 
     public static void main(String[] args) {
