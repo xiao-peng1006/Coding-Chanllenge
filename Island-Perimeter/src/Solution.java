4eb74bacb58bc6fc08460af9efcef21bb0c885f3
Xiao Peng <xiao.peng1006@gmail.com>
2019-05-04 12:14:04 -0700

    Leetcode 463. Island Perimeter     - Brutal force approach, Optimized: only check top and left neighbour     - O(n*m) Time complexity, n and m is the dimension of the grid     - O(n) space complexity

--- Island-Perimeter/src/Solution.java
+++ Island-Perimeter/src/Solution.java
@@ -6,27 +6,22 @@ class Solution {
      * @return
      */
     public int islandPerimeter(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
         int res = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
                     res += 4;
-                } else {
-                    continue;
+
+                    if (i > 0 && grid[i - 1][j] == 1)
+                        res -= 2;
+
+                    if (j > 0 && grid[i][j - 1] == 1)
+                        res -= 2;
                 }
-
-                if (i > 0 && grid[i-1][j] == 1)
-                    res -= 1;
-
-                if (i+1 < grid.length && grid[i+1][j] == 1)
-                    res -= 1;
-
-                if (j > 0 && grid[i][j-1] == 1)
-                    res -= 1;
-
-                if (j+1 < grid[0].length && grid[i][j+1] == 1)
-                    res -= 1;
-                System.out.println(res);
             }
         }
         return res;
