2191e224413eab74456f699e3fee9c1be88b20f1
Xiao Peng <xiao.peng1006@gmail.com>
2019-05-23 19:30:07 -0700

    Leetcode 442. Find All Duplicates in an Array     - Use index as location to count, if the number appears, set the value at location(Math.abs(nums[i])+1) as negative     - O(n) Time complexity, n is the size of array, nums     - O(k) Space complexity, k is the size of result

---
+++ Find-All-Duplicates-in-an-Array/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    /**
+     * Leetcode 442. Find All Duplicates in an Array
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.add(Math.abs(index + 1));
+            }
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+
+}
