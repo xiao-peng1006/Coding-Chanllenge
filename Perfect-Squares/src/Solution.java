dd0c6d86cfbe4b5fc136bd2cdd7685c4362e698b
Xiao Peng <xiao.peng1006@gmail.com>
2019-05-31 13:52:45 -0700

    Leetcode 279. Perfect Squares     - Bottom to top     - O(nlogn) Time complexity     - O(n) Space complexity

--- Perfect-Squares/src/Solution.java
+++ Perfect-Squares/src/Solution.java
@@ -1,30 +1,16 @@
 class Solution {
-    int[] memo;
 
     public int numSquares(int n) {
-        this.memo = new int[n + 1];
-        return dp(n);
-    }
-
-    private int dp(int n) {
-        if (n == 0) {
-            return 0;
+        int[] memo = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            memo[i] = Integer.MAX_VALUE;
+            for (int j = 1; j*j <= i; j++) {
+                memo[i] = Math.min(memo[i], 1 + memo[i - j * j]);
+            }
         }
-
-        if (n == 1) {
-            return 1;
-        }
-
-        if (this.memo[n] != 0) {
-            return this.memo[n];
-        }
-
-        int res = Integer.MAX_VALUE;
-        for (int i = 1; i * i <= n; i++) {
-            res = Math.min(res, 1 + dp(n - (i * i)));
-        }
-        this.memo[n] = res;
-        return res;
+        return memo[n];
     }
 
     public static void main(String[] args) {
