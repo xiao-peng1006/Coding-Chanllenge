1b313d248a04f9c9184e7d69c2378165eff1ac47
Xiao Peng <xiao.peng1006@gmail.com>
2019-05-31 12:19:44 -0700

    Leetcode 343. Integer Break     - Bottom to top     - O(n*n) Time complexity     - O(n) Space complexity

--- Integer-Break/src/Solution.java
+++ Integer-Break/src/Solution.java
@@ -2,30 +2,18 @@ class Solution {
 
     /**
      * Leetcode 343. Integer Break
-     * @param n
-     * @return
      */
-    int[] memo;
 
     public int integerBreak(int n) {
-        this.memo = new int[n + 1];
-        return breakInteger(n);
-    }
+        int[] memo = new int[n + 1];
+        memo[1] = 1;
 
-    private int breakInteger(int n) {
-        if (n == 1)
-            return 1;
-
-        if (memo[n] != 0) {
-            return memo[n];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                memo[i] = Math.max(memo[i], Math.max(j * (i - j), j * memo[i - j]));
+            }
         }
-
-        int res = -1;
-        for (int i = 1; i < n; i++) {
-            res = Math.max(res, Math.max(i * (n - i), i * breakInteger(n - i)));
-        }
-        memo[n] = res;
-        return res;
+        return memo[n];
     }
 
     public static void main(String[] args) {
