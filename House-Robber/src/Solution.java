f08bc14765d13263472add3989c92a213206ca91
Xiao Peng <xiao.peng1006@gmail.com>
2019-05-31 22:38:51 -0700

    Leetcode 198. House Robber     - O(n) Time complexity, n is the size of array, nums     - O(n) Space complexity

--- House-Robber/src/Solution.java
+++ House-Robber/src/Solution.java
@@ -1,27 +1,19 @@
 class Solution {
-    int[] memo;
-
     public int rob(int[] nums) {
-        this.memo = new int[nums.length + 1];
-        return dp(nums, 0);
-    }
-
-    public int dp(int[] nums, int start) {
-        if (start >= nums.length) {
+        if (nums == null || nums.length == 0)
             return 0;
+        if (nums.length == 1)
+            return nums[0];
+        int length = nums.length;
+        int[] dp = new int[length + 1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
         }
 
-        if (this.memo[start] != 0) {
-            return this.memo[start];
-        }
-
-        int res = 0;
-        for (int i = start; i < nums.length; i++) {
-            res = Math.max(res, nums[i] + dp(nums, i + 2));
-        }
-        this.memo[start] = res;
-
-        return res;
+        return dp[length - 1];
     }
 
     public static void main(String[] args) {
@@ -29,4 +21,5 @@ class Solution {
         System.out.println("Expected: 4, Output: " + solution.rob(new int[]{1, 2, 3, 1}));
         System.out.println("Expected: 12, Output: " + solution.rob(new int[]{2, 7, 9, 3, 1}));
     }
+
 }
