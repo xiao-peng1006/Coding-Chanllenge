d5ee43c523c1975dd48f52f8e814b86031959599
Xiao Peng <xiao.peng1006@gmail.com>
2019-06-04 23:18:30 -0700

    Leetcode 547. Friend Circles     - DFS     - O(n*n) Time complexity, n is the size of array, M(n by n)     - O(n) Space complexity

---
+++ Friend-Circles/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+    int[][] M;
+
+    public int findCircleNum(int[][] M) {
+        this.M = M;
+        int[] visited = new int[M.length];
+        int res = 0;
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dfs(visited, i);
+                res++;
+            }
+        }
+        return res;
+
+    }
+
+    public void dfs(int[] visited, int i) {
+        for (int j = 0; j < this.M.length; j++) {
+            if (this.M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(visited, j);
+            }
+        }
+    }
+}
