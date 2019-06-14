464ef4667e83813b901425de1fbdf33e626ca4af
Xiao Peng <xiao.peng1006@gmail.com>
2019-06-14 11:42:37 -0700

    Leetcode 287. Find the Duplicate Number     - Optimized: Use negativity to track on duplication     - O(n) Time complexity, n is the size of array, nums     - O(1) Space complexity

--- Find-the-Duplicate-Number/src/Solution.java
+++ Find-the-Duplicate-Number/src/Solution.java
@@ -5,11 +5,11 @@ class Solution {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return nums[i];
-                }
+            int index = Math.abs(nums[i]);
+            if (nums[index] < 0) {
+                return index;
             }
+            nums[index] *= -1;
         }
         return 0;
     }
