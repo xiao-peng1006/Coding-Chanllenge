831df9f1052f2d042aa13fbc9e858334237d0120
Xiao Peng <xiao.peng1006@gmail.com>
2019-06-21 23:32:28 -0700

    Leetcode 621. Task Scheduler     - Count the idle slots     - O(nlogn) Time complexity, n is the size of array, tasks     - O(1) Space complexity

---
+++ Task-Scheduler/src/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char task : tasks) {
+            map[task - 'A']++;
+        }
+        Arrays.sort(map);
+        int max_val = map[25] - 1, idle_slots = max_val * n;
+        for (int i = 24; i >= 0; i--) {
+            idle_slots -= Math.min(map[i], max_val);
+        }
+        return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
+    }
+}
