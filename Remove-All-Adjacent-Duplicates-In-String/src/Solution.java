162335ec65bdccc74f779bcb84e8438b0d9ae4a7
Xiao Peng <xiao.peng1006@gmail.com>
2019-08-19 18:26:23 -0700

    Leetcode 1047. Remove All Adjacent Duplicates In String     - Optimized by using stack     - O(n) Time complexity, n is the length of String     - O(n) Space complexity

--- Remove-All-Adjacent-Duplicates-In-String/src/Solution.java
+++ Remove-All-Adjacent-Duplicates-In-String/src/Solution.java
@@ -1,16 +1,26 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
 class Solution {
     public String removeDuplicates(String S) {
-        for (int i = 1; i < S.length(); i++) {
-            if (S.charAt(i) == S.charAt(i - 1)) {
-                S = S.substring(0, i - 1) + S.substring(i + 1, S.length());
-                i = 0;
+        Stack<Character> stack = new Stack<>();
+        for (Character c : S.toCharArray()) {
+            if (!stack.empty() && stack.peek().equals(c)) {
+                stack.pop();
+            } else {
+                stack.push(c);
             }
         }
-        return S;
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.removeDuplicates("aaaaa"));
+        System.out.println(solution.removeDuplicates("abbaca"));
     }
 }
