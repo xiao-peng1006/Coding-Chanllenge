6ca9f7200a26f7b13fee54a686b77ca412ea2bab
Xiao Peng <xiao.peng1006@gmail.com>
2019-09-16 15:12:13 -0700

    Leetcode 80. Remove Duplicates from Sorted Array II     - use two pointer     - O(n) Time complexity, n is the size of array, nums     - O(1) Space complexity

--- Remove-Duplicates-from-Sorted-Array-II/src/Solution.java
+++ Remove-Duplicates-from-Sorted-Array-II/src/Solution.java
@@ -1,16 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int end = nums.length;
-        for (int i = 2; i < end; i++) {
-            if (nums[i] == nums[i - 2]) {
-                for (int j = i + 1; j < end; j++) {
-                    nums[j - 1] = nums[j];
-                }
-                end--;
-                i--;
+        int k = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] > nums[k-2]) {
+                nums[k++] = nums[i];
             }
-
         }
-        return end;
+        return k;
     }
 }
